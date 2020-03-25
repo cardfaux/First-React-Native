740f7139f0a9f5c2686f87d97bc060366cfdcab8
James Hagood <cardfaux@tutanota.com>
2020-03-24 20:12:44 -0500

    got android studio set up

--- App.js
+++ App.js
@@ -1,19 +1,26 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Button } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+	const [outputText, setOutputText] = useState(
+		'Open up App.js to start working!'
+	);
+	return (
+		<View style={styles.container}>
+			<Text>{outputText}</Text>
+			<Button
+				title='Change Text'
+				onPress={() => setOutputText('The Text Changed!')}
+			/>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#fff',
+		alignItems: 'center',
+		justifyContent: 'center'
+	}
 });
