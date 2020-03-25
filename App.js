8274af6a9c75b880ffc788677a1e068d3d44fbfe
James Hagood <cardfaux@tutanota.com>
2020-03-25 10:04:42 -0500

    added to do logic

--- App.js
+++ App.js
@@ -1,26 +1,71 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import {
+	StyleSheet,
+	Text,
+	View,
+	Button,
+	TextInput,
+	FlatList
+} from 'react-native';
 
 export default function App() {
-	const [outputText, setOutputText] = useState(
-		'Open up App.js to start working!'
-	);
+	const [enteredGoal, setEnteredGoal] = useState('');
+	const [courseGoals, setCourseGoals] = useState([]);
+
+	const goalInputHandler = (enteredText) => {
+		setEnteredGoal(enteredText);
+	};
+
+	const addGoalHandler = () => {
+		setCourseGoals((currentGoals) => [
+			...currentGoals,
+			{ key: Math.random().toString(), value: enteredGoal }
+		]);
+	};
+
 	return (
-		<View style={styles.container}>
-			<Text>{outputText}</Text>
-			<Button
-				title='Change Text'
-				onPress={() => setOutputText('The Text Changed!')}
+		<View style={styles.screen}>
+			<View style={styles.inputContainer}>
+				<TextInput
+					placeholder='Course Goal'
+					style={styles.input}
+					onChangeText={goalInputHandler}
+					value={enteredGoal}
+				/>
+				<Button title='ADD' onPress={addGoalHandler} />
+			</View>
+			<FlatList
+				data={courseGoals}
+				renderItem={(itemData) => (
+					<View style={styles.listItem}>
+						<Text>{itemData.item.value}</Text>
+					</View>
+				)}
 			/>
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center'
+	screen: {
+		padding: 60
+	},
+	inputContainer: {
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		alignItems: 'center'
+	},
+	input: {
+		width: '80%',
+		borderColor: 'black',
+		borderWidth: 1,
+		padding: 10
+	},
+	listItem: {
+		padding: 10,
+		marginVertical: 10,
+		backgroundColor: '#ccc',
+		borderColor: 'black',
+		borderWidth: 1
 	}
 });
